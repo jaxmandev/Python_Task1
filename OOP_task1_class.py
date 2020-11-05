d86de9d0b094097f843ea55bf44508244f32c4a6
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-05 21:40:09 +0000

    Indentation fix

--- OOP_task1_class.py
+++ OOP_task1_class.py
@@ -5,17 +5,15 @@ class test_of_multiples():
         self.range_end = range_end
         self.fizz = fizz
         self.buzz = buzz
-
-# Test the divisibility
-        def fizzbuzz(self): #method
-            list = []
-            for i in range(self.range_start, self.range_end + 1):
-                if i % self.fizz == 0 and i % self.buzz == 0: #condition for multiple of both numbers
-                    list.append("FizzBuzz")
-                elif i % self.fizz == 0: 
-                    list.append("Fizz")
-                elif i % self.buzz == 0: 
-                    list.append("Buzz")
-                else:
-                    list.append(i)
-            return list
+     def fizzbuzz(self): #method
+        list = []
+        for i in range(self.range_start, self.range_end + 1):
+            if i % self.fizz == 0 and i % self.buzz == 0: #condition for multiple of both numbers
+                list.append("FizzBuzz")
+            elif i % self.fizz == 0:
+                list.append("Fizz")
+            elif i % self.buzz == 0:
+                list.append("Buzz")
+            else:
+                list.append(i)
+        return list
