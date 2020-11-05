ba67ac8c7528bdb56836d0b025eeecd329505bac
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-05 21:45:44 +0000

    Update OOP_task1_class.py

--- OOP_task1_class.py
+++ OOP_task1_class.py
@@ -1,14 +1,13 @@
 class test_of_multiples():
     def __init__(self, range_start, range_end, fizz, buzz):
-        #initialise the class with 4 attributes excl self
         self.range_start = range_start
         self.range_end = range_end
         self.fizz = fizz
         self.buzz = buzz
-     def fizzbuzz(self):
+    def fizzbuzz(self):
         list = []
         for i in range(self.range_start, self.range_end + 1):
-            if i % self.fizz == 0 and i % self.buzz == 0: #condition for multiple of both numbers
+            if i % self.fizz == 0 and i % self.buzz == 0:
                 list.append("FizzBuzz")
             elif i % self.fizz == 0:
                 list.append("Fizz")
