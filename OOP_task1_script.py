df3833cd9418239e5a5a36c33073395b75aa9165
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-05 21:35:01 +0000

    Update OOP_task1_script.py

--- OOP_task1_script.py
+++ OOP_task1_script.py
@@ -6,7 +6,7 @@ range_start = int(input('Where would you like to start?'))
 range_end = int(input('Where would you like to end?'))
 
 if 0 not in (range_start, range_end, fizz, buzz):
-    object = test_of_multiples(fizz, buzz, range_start, range_end)
+    object = test_of_multiples(range_start, range_end, fizz, buzz)
     print(object.fizzbuzz())
 else:
     print('0 cannot be in the range of start, end nor can it be Fizz or Buzz, please try another input.')
