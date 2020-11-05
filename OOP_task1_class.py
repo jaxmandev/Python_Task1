c3f741bfb073c12949742c28a3a5c6d2794b3bb2
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-05 21:02:41 +0000

    Create OOP_task1_class.py

---
+++ OOP_task1_class.py
@@ -0,0 +1,21 @@
+class test_of_multiples:
+    def __init__(self, range_start, range_end, fizz, buzz):
+        #initialise the class with 4 attributes excl self
+        self.range_start = range_start
+        self.range_end = range_end
+        self.fizz = fizz
+        self.buzz = buzz
+
+# Test the divisibility
+        def fizzbuzz(self): #method
+            list = []
+            for i in range(self.range_start, self.range_end + 1):
+                if i % self.fizz == 0 and i % self.buzz == 0: #condition for multiple of both numbers
+                    list.append("FizzBuzz")
+                elif i % self.fizz == 0: 
+                    list.append("Fizz")
+                elif i % self.buzz == 0: 
+                    list.append("Buzz")
+                else:
+                    list.append(i)
+            return(number)
