06747f76bd05742835d6b9159ea7a379faaa39d9
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-05 21:47:46 +0000

    Update OOP_task1_script.py

--- OOP_task1_script.py
+++ OOP_task1_script.py
@@ -1,12 +1,12 @@
 from OOP_task1_class import *
 
-fizz = int(input('What number would you like to replace multiples of with "Fizz"?'))
-buzz = int(input('What number would you like to replace multiples of with "Buzz"?'))
-range_start = int(input('Where would you like to start?'))
-range_end = int(input('Where would you like to end?'))
+fizz = int(input('What number would you like to replace multiples of with "Fizz"? '))
+buzz = int(input('What number would you like to replace multiples of with "Buzz"? '))
+range_start = int(input('Where would you like to start? '))
+range_end = int(input('Where would you like to end? '))
 
 if 0 not in (range_start, range_end, fizz, buzz):
     object = test_of_multiples(range_start, range_end, fizz, buzz)
     print(object.fizzbuzz())
 else:
-    print('0 cannot be in the range of start, end nor can it be Fizz or Buzz, please try another input.')
+    print('0 is not appropriate please try another input.')
